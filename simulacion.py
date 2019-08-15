b73a26c33e1698c2d7ee8cf1380848b19de6b4b5
Santiago Maldonado Ochoa <santimaldonadoochoa@gmail.com>
2019-08-15 00:10:27 -0300

    funcion el pulso de 90

--- simulacion.py
+++ simulacion.py
@@ -4,53 +4,78 @@ import numpy as np
 def extraer_resultados(archivo):
     # se usa asi:
     # [X, Y, Z, Bx, By, Bz] = self.extraer_resultados()
-    
+
     resultados = np.loadtxt(archivo, comments='%', unpack=True)
     # elimino las filas que continenen NaN, para ello debo transponer.
     resultados = resultados.transpose()
-    resultados = resultados[~np.isnan(resultados).any(axis=1)]                        
-    
+    resultados = resultados[~np.isnan(resultados).any(axis=1)]
+
     #print("resultados '"+self.archivo+"' extraidos con exito.")
 
     #self.resultados = {'X': X, 'Y': Y, 'Z': Z,  'Bx': Bx, 'By': By, 'Bz': Bz}
-    # el return esta transpuesto para poder usarlo asi: 
+    # el return esta transpuesto para poder usarlo asi:
     # [X, Y, ...] = self.extraer_resultados()
     return resultados.transpose()
 
 
 def nutacion(Bx,By):
-    H, xedges, yedges = np.histogram2d(Bx,By, bins=500)
+    H, xedges, yedges = np.histogram2d(Bx,By, bins=2001)
 
     indices = np.where(H == H.max())
     Bx90 = np.mean(xedges[indices[0][0]:indices[0][0]+2])
     By90 = np.mean(yedges[indices[1][0]:indices[1][0]+2])
 
-    tp = np.pi / 2 / abs(Bx90 + 1j * By90)
+    B90 = np.array([Bx90, By90])
+    print(B90)
+    B90 = np.linalg.norm(B90)
+
+    tp = np.pi / 2 / B90
+
+    print(B90)
 
     return tp
 
 def Pulso90(Bx, By, Bz, tp):
     B1 = np.array([Bx, By, Bz])
     B1 = np.linalg.norm(B1, axis=0)
-    
+
     Ux = Bx/B1
     Uy = By/B1
     Uz = Bz/B1
 
-    R = generar_matriz_R(Ux, Uy, Uz, tp)
+    angulo = B1*tp
+    print("angulo: ", angulo)
+    R = generar_matriz_R(Ux, Uy, Uz, angulo)
     M0 = np.array([0,0,1])
-    
+
     M = np.dot(R,M0)
     return M
 
-def generar_matriz_R(Ux, Uy, Uz, tp):
-    O = np.zeros_like(Ux)
-    I = np.ones_like(Ux)
-    
-    
-    
+def generar_matriz_R(Ux, Uy, Uz, angulo):
+    zeros = np.zeros_like(Ux)
+    ones = np.ones_like(Ux)
 
-    
+    U_matrix = np.array([[ zeros, -Uz   ,  Uy   ],
+                         [ Uz   ,  zeros, -Ux   ],
+                         [-Uy   ,  Ux   ,  zeros]]
+                        )
+
+    Uxy, Uxz, Uyz = [Ux*Uy, Ux*Uz, Uy*Uz]
+    U2_matrix = np.array([[Ux*Ux, Uxy  , Uxz  ],
+                          [Uxy  , Uy*Uy, Uyz  ],
+                          [Uxz  , Uyz  , Uz*Uz]]
+                         )
+
+    I = np.array([[ones, zeros, zeros], [zeros, ones, zeros], [zeros, zeros, ones]])
+
+    R = np.cos(angulo) * I + np.sin(angulo) * U_matrix + (1-np.cos(angulo)) * U2_matrix
+    # convierto en array nx3x3
+    R = np.moveaxis(R,2,0)
+    return R
+
+
+
+#---------------------FUNCIONES DE PRUEBA
 def Rot_y(vector,angulo):
     # definir matriz de rotacion
     c = np.cos(angulo)
@@ -64,41 +89,39 @@ def Rot_y(vector,angulo):
     print('shape Ry: ', Ry.shape)
     #rint('Ry: ', Ry)
     return np.dot(Ry, vector)
-
-
-def Rot_y(vector,angulo):
-    # definir matriz de rotacion
-    c = np.cos(angulo)
-    print('shape cos(angulo): ', c.shape)
-    s = np.sin(angulo)
-    O = np.zeros_like(angulo)
-    I = np.ones_like(angulo)
-    Ry = np.array([[c, O, s], [O, I, O], [-s, O, c]])
-    
-    print('shape Ry: ', Ry.shape)
-    Ry = np.rollaxis(Ry,-1)
-    print('shape Ry: ', Ry.shape)
-    #rint('Ry: ', Ry)
-    return np.dot(Ry, vector)
-
 
 def Rot(kx, ky, kz, angulo):
     #kx = direccion[:,0]
     #ky = direccion[:,1]
     #kz = direccion[:,2]
+    """
+    NO LOGRE HACER QUE FUNCIONE LA FORMULA DE RODRIGUES, TENDRE QUE HARDCODEAR
+    """
     print('rotacion...')
     print(kx, ky, kz)
     zeros = np.zeros_like(kx)
     ones = np.ones_like(kx)
-    
-    K = np.array([[zeros, -kz, ky], [kz, zeros, -kx], [-ky, kx, zeros]])
-    K = np.rollaxis(K, -1)
-    I = np.array([[ones, zeros, zeros], [zeros, ones, zeros], [zeros, zeros, ones]])
-    I = np.rollaxis(I, -1)
-    print('I:', I, I.shape)
-    
-    R = I + np.sin(angulo) * K + (1-np.cos(angulo)) * K**2
-    
 
+    K = np.array([[zeros, -kz   ,  ky   ],
+                  [kz   ,  zeros, -kx  ],
+                  [-ky  ,  kx   ,  zeros]]
+                 )
+    #K = np.moveaxis(K, 2, 0)
+
+    kxy, kxz, kyz = [kx*ky, kx*kz, ky*kz]
+    KxK = np.array([[kx*kx, kxy  , kxz  ],
+                    [kxy  , ky*ky, kyz  ],
+                    [kxz  , kyz  , kz*kz]])
+    #KxK = np.moveaxis(KxK, 2, 0)
+    I = np.array([[ones, zeros, zeros], [zeros, ones, zeros], [zeros, zeros, ones]])
+    #I = np.moveaxis(I, 2, 0)
+    #print('I:', I, I.shape)
+
+    cos = np.cos(angulo)
+    sin = np.sin(angulo)
+
+    R = cos * I + sin * K + (1-cos) * KxK
+    R = np.moveaxis(R,2,0)
     M0 = np.array([0,0,1])
-    return np.dot(R, M0)
+    M = np.dot(R, M0)
+    return M, I, K, KxK, R
---
+++ test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from simulacion import *
+
+# kx = np.array([0, 0, 1, -1])
+# ky = np.array([0, 1, 0, 0])
+# kz = np.array([1, 0, 0, 0])
+#angulo = np.pi/2
+kx = np.zeros(4)
+ky = np.ones(4)
+kz = np.zeros(4)
+angulo = np.array([0.25, 0.5, 1, 1]) * np.pi
+
+M, I, K, KxK,  R = Rot(kx,ky,kz, angulo)
+
+# para visualizar mas facil:
+M[np.abs(M)<1e-10] = 0
+print('vector rotado: \n', M)
+
+print('-----------------------------------')
+
+# ahora pruebo las funciones posta
+# supongamos un campo en x. COnsideremos 10 sitios, en todos el cualquiera, por ejemplo, 273
+sitios = 10
+campo = 273.0
+Bx = np.ones(sitios) * campo
+By = np.zeros(sitios)
+Bz = np.zeros(sitios)
+
+# hago una nutacion
+tp = nutacion(Bx,By)
+
+#aplico Pulso
+M = Pulso90(Bx, By, Bz, tp)
+
+# para visualizar mas facil:
+M[np.abs(M)<1e-10] = 0
